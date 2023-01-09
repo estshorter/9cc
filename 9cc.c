9289e6a812c111465a79802a7eda0fdc5c1a530b
estshorter <1430311+estshorter@users.noreply.github.com>
2023-01-09 16:28:06 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -25,12 +25,19 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -55,7 +62,7 @@ void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
     {
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -66,7 +73,7 @@ int64_t expect_number()
 {
     if (token->kind != TK_NUM)
     {
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
 
     int64_t val = token->val;
@@ -118,7 +125,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
@@ -132,6 +139,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    user_input = argv[1];
     // トークナイズする
     token = tokenize(argv[1]);
 
